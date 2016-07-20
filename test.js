c42fade5841a3c939e0d2e18f7bddd0c0615824c
YerkoPalma <yerko.palma@usach.cl>
2016-07-19 22:32:25 -0400

    Prepare for tests
    * Removed unused deps
    * Fixed typo to pass standard check

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 const test = require('tape')
-const choo-log = require('./')
+const chooLog = require('./')
 
 test('should assert input types', function (t) {
   t.plan(1)
-  t.throws(choo-log)
+  t.throws(chooLog)
 })

65d56eeec5a5e91e6fe187b34cfa50390cecd5a5
IzumiSy <beetle-noise@gmx.com>
2017-10-18 15:28:59 +0900

    Add LogOpts interface

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,9 @@
 export default logger;
 
-declare function logger(): (state: any, emitter: any) => void;
+declare namespace Choo {
+  export interface LogOpts {
+    colors?: object
+  }
+}
+
+declare function logger(opts?: Choo.LogOpts): (state: any, emitter: any) => void;
